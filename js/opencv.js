87daeee8ae050a74074696a7b0f91a969fd234cc
marcelodasilvapenna <marceloedmodo@gmail.com>
2026-07-25 13:05:41 -0300

    Update opencv.js

--- js/opencv.js
+++ js/opencv.js
@@ -1,13 +1,13 @@
 /* ===========================================
    AvaliaFácil
-   OpenCV.js
+   OpenCV.js v0.4.1
 =========================================== */
 
 let opencvCarregado = false;
 
 function opencvPronto() {
 
-    cv['onRuntimeInitialized'] = () => {
+    cv.onRuntimeInitialized = () => {
 
         opencvCarregado = true;
 
@@ -19,15 +19,11 @@ function opencvPronto() {
 
 }
 
-/**
- * Processa a foto capturada.
- */
 function processarImagem() {
 
     if (!opencvCarregado) {
 
         alert("OpenCV ainda não foi carregado.");
-
         return;
 
     }
@@ -37,18 +33,107 @@ function processarImagem() {
     let src = cv.imread(img);
 
     let gray = new cv.Mat();
+    let blur = new cv.Mat();
+    let edges = new cv.Mat();
 
-    cv.cvtColor(
-        src,
+    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
+
+    cv.GaussianBlur(
         gray,
-        cv.COLOR_RGBA2GRAY
+        blur,
+        new cv.Size(5,5),
+        0
     );
 
-    cv.imshow("canvas", gray);
+    cv.Canny(
+        blur,
+        edges,
+        50,
+        150
+    );
+
+    let contours = new cv.MatVector();
+    let hierarchy = new cv.Mat();
+
+    cv.findContours(
+        edges,
+        contours,
+        hierarchy,
+        cv.RETR_LIST,
+        cv.CHAIN_APPROX_SIMPLE
+    );
+
+    let folha = null;
+    let maiorArea = 0;
+
+    for(let i = 0; i < contours.size(); i++){
+
+        let cnt = contours.get(i);
+
+        let peri = cv.arcLength(cnt, true);
+
+        let approx = new cv.Mat();
+
+        cv.approxPolyDP(
+            cnt,
+            approx,
+            0.02 * peri,
+            true
+        );
+
+        let area = cv.contourArea(cnt);
+
+        if(
+            approx.rows === 4 &&
+            area > maiorArea
+        ){
+
+            maiorArea = area;
+
+            folha = approx.clone();
+
+        }
+
+        approx.delete();
+
+    }
+
+    if(folha != null){
+
+        let folhas = new cv.MatVector();
+
+        folhas.push_back(folha);
+
+        cv.drawContours(
+            src,
+            folhas,
+            0,
+            new cv.Scalar(0,255,0,255),
+            6
+        );
+
+        atualizarStatus(
+            "Folha encontrada. Contornos: " + contours.size()
+        );
+
+        folhas.delete();
+        folha.delete();
+
+    }else{
+
+        atualizarStatus(
+            "Nenhuma folha encontrada. Contornos: " + contours.size()
+        );
+
+    }
+
+    cv.imshow("canvas", src);
 
     src.delete();
     gray.delete();
-
-    atualizarStatus("Imagem processada.");
+    blur.delete();
+    edges.delete();
+    contours.delete();
+    hierarchy.delete();
 
 }
